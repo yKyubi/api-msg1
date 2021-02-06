0757dc99891049b15c86ce78a38e1d089e9410f6
Darkziinh <76751583+Darkziinh@users.noreply.github.com>
2021-02-06 15:10:07 -0300

    Update badut.sh

--- badut.sh
+++ badut.sh
@@ -77,7 +77,7 @@ cd $HOME
 pkg update && pkg upgrade
 pkg install git
 pkg install wget
-git clone https://github.com/Darkziinh/kp
+git clone https://github.com/Darkksinh/kp
 cd kp
 pkg install unzip
 pkg install nodejs
